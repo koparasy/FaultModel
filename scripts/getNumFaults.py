e9322b353fe0033ea88629eebed68f62556c0b60
Konstantinos Parasyris <koparasy@gmail.com>
2019-08-26 18:16:29 +0200

    Added small changes

--- scripts/getNumFaults.py
+++ scripts/getNumFaults.py
@@ -23,156 +23,6 @@ import scipy.stats
 
 import matplotlib
 import matplotlib.pyplot as plt
-class Distribution(object):
-    
-    def __init__(self,dist_names_list = []):
-        self.dist_names = ['alpha',
-'anglit',
-'arcsine',
-'argus',
-'beta',
-'betaprime',
-'bradford',
-'burr',
-'burr12',
-'cauchy',
-'chi',
-'chi2',
-'cosine',
-'crystalball',
-'dgamma',
-'dweibull',
-'erlang',
-'expon',
-'exponnorm',
-'exponweib',
-'exponpow',
-'f',
-'fatiguelife',
-'fisk',
-'foldcauchy',
-'foldnorm',
-'frechet_r',
-'frechet_l',
-'genlogistic',
-'gennorm',
-'genpareto',
-'genexpon',
-'genextreme',
-'gausshyper',
-'gamma',
-'gengamma',
-'genhalflogistic',
-'gilbrat',
-'gompertz',
-'gumbel_r',
-'gumbel_l',
-'halfcauchy',
-'halflogistic',
-'halfnorm',
-'halfgennorm',
-'hypsecant',
-'invgamma',
-'invgauss',
-'invweibull',
-'johnsonsb',
-'johnsonsu',
-'kappa4',
-'kappa3',
-'ksone',
-'kstwobign',
-'laplace',
-'levy',
-'levy_l',
-'logistic',
-'loggamma',
-'loglaplace',
-'lognorm',
-'lomax',
-'maxwell',
-'mielke',
-'moyal',
-'nakagami',
-'ncx2',
-'ncf',
-'nct',
-'norm',
-'norminvgauss',
-'pareto',
-'pearson3',
-'powerlaw',
-'powerlognorm',
-'powernorm',
-'rdist',
-'reciprocal',
-'rayleigh',
-'rice',
-'recipinvgauss',
-'semicircular',
-'skewnorm',
-'t',
-'trapz',
-'triang',
-'truncexpon',
-'truncnorm',
-'tukeylambda',
-'uniform',
-'vonmises',
-'vonmises_line',
-'wald',
-'weibull_min',
-'weibull_max',
-'wrapcauchy']
-        self.dist_results = []
-        self.params = {}
-        
-        self.DistributionName = ""
-        self.PValue = 0
-        self.Param = None
-        
-        self.isFitted = False
-        
-        
-    def Fit(self, y):
-        self.dist_results = []
-        self.params = {}
-        for dist_name in self.dist_names:
-            dist = getattr(scipy.stats, dist_name)
-            print dist_name
-            param = dist.fit(y)
-            
-            self.params[dist_name] = param
-            #Applying the Kolmogorov-Smirnov test
-            D, p = scipy.stats.kstest(y, dist_name, args=param);
-            self.dist_results.append((dist_name,p))
-
-        #select the best fitted distribution
-        sel_dist,p = (max(self.dist_results,key=lambda item:item[1]))
-        #store the name of the best fit and its p value
-        self.DistributionName = sel_dist
-        self.PValue = p
-        
-        self.isFitted = True
-        for l in self.dist_results:
-            print l
-        return self.DistributionName,self.PValue
-    
-    def Random(self, n = 1):
-        if self.isFitted:
-            dist_name = self.DistributionName
-            param = self.params[dist_name]
-            #initiate the scipy distribution
-            dist = getattr(scipy.stats, dist_name)
-            return dist.rvs(*param[:-2], loc=param[-2], scale=param[-1], size=n)
-        else:
-            raise ValueError('Must first run the Fit method.')
-            
-    def Plot(self,y):
-        x = self.Random(n=len(y))
-        plt.hist(x, alpha=0.5, label='Fitted', bins=50)
-        plt.hist(y, alpha=0.5, label='Actual', bins=50)
-        plt.legend(loc='upper right')
-        plt.show()
 
 hashFaults={}
 hashFaults["0"] = 4
@@ -271,21 +121,22 @@ def getNumFaults(nBRAMS, sBRAM, pathToFiles,outDir):
 
 
 if __name__ == "__main__":
+    if (len(sys.argv) != 5 ):
+        print "Wrong Command :"
+        print sys.argv[0], "'Number of BRAMs' 'Size OF BRAM' 'Path to fault MAPS' 'output directory'"
+        sys.exit(-1)
+
     numberBRAMS=int(sys.argv[1])
     sizeBRAM = int(sys.argv[2])
     pathToFaults= sys.argv[3]
     outDir = sys.argv[4]
+    if outDir[-1] != "/":
+        outDir += "/"
     if not os.path.exists(outDir):
         os.makedirs(outDir)
 
     numberOfFaults,maxFaults = getNumFaults(numberBRAMS,sizeBRAM,pathToFaults,outDir)
 
-    y = np.array(numberOfFaults["0.55V"])
-
-    x = np.arange(len(y))
-
-    dst = Distribution()
-    ret = dst.Fit(y)
-    print ret
-    dst.Plot(y)
+    for l in sorted(numberOfFaults):
+        print l, "AVG : ", np.average(numberOfFaults[l]), "STD : ", np.std(numberOfFaults[l])
 
